deea6a85280858cb0dc805cae41a0d6d12ba85dd
valix <103791978+LukeyWentToLuki@users.noreply.github.com>
2024-09-04 13:22:28 -0400

    Update exec.lua

--- exec.lua
+++ exec.lua
@@ -1,1 +1,1 @@
-
+loadstring(game:HttpGet("https://gist.githubusercontent.com/LukeyWentToLuki/6a52d7bb6c770cd4eb2f33951ca8b2ed/raw/31920b0fa2a83bac3460d34e5b584a2eecb5e958/customCapesForBridgeDuels.lua"))()
